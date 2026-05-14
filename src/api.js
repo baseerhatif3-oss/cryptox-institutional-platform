6f91cebecc25839a6a88be6c8436b0e11ad7aa1d
Abdul Baseer <Baseer@Abduls-MacBook-Pro.local>
2026-05-14 09:43:11 +0500

    fix api

--- src/api.js
+++ src/api.js
@@ -5,4 +5,21 @@ const API = axios.create({
     "https://crypto-platform-backend-d2az.onrender.com/api",
 });
 
+API.interceptors.request.use(
+  (req) => {
+    const userInfo =
+      JSON.parse(
+        localStorage.getItem(
+          "userInfo"
+        )
+      );
+
+    if (userInfo?.token) {
+      req.headers.Authorization = `Bearer ${userInfo.token}`;
+    }
+
+    return req;
+  }
+);
+
 export default API;
