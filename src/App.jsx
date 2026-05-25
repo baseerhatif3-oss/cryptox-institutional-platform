3fdc1d6b91573d7bc4daa30e4b3b304efcb8d0bb
Abdul Baseer <Baseer@Abduls-MacBook-Pro.local>
2026-05-25 07:30:41 +0500

    Debug production crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 
+import Dashboard from "./pages/Dashboard";
+
 function App() {
 
-  return (
-
-    <div className="min-h-screen bg-black text-white flex items-center justify-center text-5xl font-bold">
-
-      CryptoX Working 🚀
-
-    </div>
-  );
+  return <Dashboard />;
 }
 
 export default App;
