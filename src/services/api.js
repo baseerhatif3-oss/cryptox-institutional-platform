c0dd1ce101551c94a2499856f2aef3d8737311ac
Abdul Baseer <Baseer@Abduls-MacBook-Pro.local>
2026-05-25 08:12:04 +0500

    Connect frontend to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,9 @@ const API =
   axios.create({
 
     baseURL:
-      "http://127.0.0.1:5000/api",
+      "https://crypto-backend-dojp.onrender.com/api",
+
+    withCredentials: true,
   });
 
 export default API;
