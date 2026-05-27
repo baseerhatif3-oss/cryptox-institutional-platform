0740c43f8e86399b54fb315a78f689355ee27c99
Abdul Baseer <Baseer@Abduls-MacBook-Pro.local>
2026-05-27 09:25:39 +0500

    Absolute frontend isolation fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+function Test() {
 
-import "./index.css";
+  return (
+
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "48px",
+        fontWeight: "bold",
+      }}
+    >
+
+      WEBSITE WORKING 🚀
+
+    </div>
+  );
+}
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Test />
 );
